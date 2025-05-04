df8ce62be4da1596f23065b98743308ae1e5139a
jellyfish-cat <ducp9531@gmail.com>
2025-05-04 10:34:30 +0700

    upload lần 3

--- obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0bd6aaf8ce4a062fa8cf05a6457314e3797f5ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457c5e65936fa0c90fcb59cd3180a48bad8755f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
